c20e18324ceaf48ed1acaff04194af3aa4d851b7
Paul Farning <paul@waytoocrowded.com>
2014-09-02 15:13:31 -0700

    Authomatic

--- config.py
+++ config.py
@@ -1,9 +1,44 @@
 import collections
 import jinja2
 import webapp2
+from authomatic.providers import oauth2, oauth1, gaeopenid
 
 JINJA_ENVIRONMENT = jinja2.Environment(
   loader=jinja2.FileSystemLoader('views'),
   extensions=['jinja2.ext.autoescape'],
   autoescape=True)
 JINJA_ENVIRONMENT.globals['OrderedDict'] = collections.OrderedDict
+
+AUTHOMATIC_CONFIG = {
+
+    'tw': { # Your internal provider name
+
+        # Provider class
+        'class_': oauth1.Twitter,
+
+        # Twitter is an AuthorizationProvider so we need to set several other properties too:
+        'consumer_key': '########################',
+        'consumer_secret': '########################',
+    },
+
+    'fb': {
+
+        'class_': oauth2.Facebook,
+
+        # Facebook is an AuthorizationProvider too.
+        'consumer_key': '########################',
+        'consumer_secret': '########################',
+
+        # But it is also an OAuth 2.0 provider and it needs scope.
+        'scope': ['user_about_me', 'email', 'publish_stream', 'read_stream'],
+    },
+
+    'gae_oi': {
+
+        # OpenID provider based on Google App Engine Users API.
+        # Works only on GAE and returns only the id and email of a user.
+        # Moreover, the id is not available in the development environment!
+        'class_': gaeopenid.GAEOpenID,
+    }
+
+}
--- main.py
+++ main.py
@@ -1,13 +1,48 @@
+import sys
+sys.path.insert(0, 'libs')
+
 from controllers.home import Main as MainHome
 from controllers.patches import Main as MainPatches
 from controllers.patches import Create as CreatePatches
 from controllers.patches import View as ViewPatches
 from controllers.admin import Main as MainAdmin
 
+from authomatic import Authomatic
+from authomatic.adapters import Webapp2Adapter
+
+from config import AUTHOMATIC_CONFIG
+
 import webapp2
+
+authomatic = Authomatic(config=AUTHOMATIC_CONFIG, secret='yumyumrandomstring')
+
+class Login(webapp2.RequestHandler):
+  def get(self, provider_name):
+    result = authomatic.login(Webapp2Adapter(self), provider_name)
+    if result:
+      self.response.write('<a href="..">Home</a>')
+
+      if result.error:
+        self.response.write(u'<h2>Damn that error: {}</h2>'.format(result.error.message))
+      elif result.user:
+        if not (result.user.name and result.user.id):
+          result.user.update()
+
+        self.response.write(u'<h1>Hi {}</h1>'.format(result.user.name))
+        self.response.write(u'<h2>Your id is: {}</h2>'.format(result.user.id))
+        self.response.write(u'<h2>Your email is: {}</h2>'.format(result.user.email))
+
+        if result.user.credentials:
+          if result.provider.name == 'fb':
+            self.response.write('Your are logged in with Facebook.<br />')
+
+          if result.provider.name == 'tw':
+              self.response.write('Your are logged in with Twitter.<br />')
+
 
 app = webapp2.WSGIApplication([
   (r'/', MainHome),
+  (r'/login/([^\/]*)', Login),
   (r'/synths', MainAdmin),
   (r'/patches', MainPatches),
   (r'/patches/create', CreatePatches),
